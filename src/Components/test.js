12a58664f364097391c7be9526a9713338168a4a
rizwan3749 <rizwan9911k@gmail.com>
2024-08-22 16:18:50 +0530

    update

--- src/Components/test.js
+++ src/Components/test.js
@@ -4,25 +4,25 @@ const testimonials = () => {
       name: "Mohd Afzal",
       company: "First India Credit",
       testimonial:
-        "The web app service provided by Md Sharik was exceptional. Our CRM app is fast, responsive, and visually appealing. It has significantly improved our online presence and client engagement.",
+        "The web app service provided by Rizwan was exceptional. Our CRM app is fast, responsive, and visually appealing. It has significantly improved our online presence and client engagement.",
     },
     {
       name: "Matin",
       company: "IdeareBuzz",
       testimonial:
-        "Working with Md Sharik was a great experience. The website he developed for us exceeded our expectations in terms of design and functionality. It has greatly enhanced our brand's image.",
+        "Working with Rizwan was a great experience. The website he developed for us exceeded our expectations in terms of design and functionality. It has greatly enhanced our brand's image.",
     },
     {
       name: "Paul",
       company: "Paul Walter",
       testimonial:
-        "Md Sharik's web development service was outstanding. The new site is user-friendly, professional, and has helped us attract more visitors and patients. We couldn't be happier with the results.",
+        "Rizwan web development service was outstanding. The new site is user-friendly, professional, and has helped us attract more visitors and patients. We couldn't be happier with the results.",
     },
     {
       name: "Somnath",
       company: "SP Digital",
       testimonial:
-        "The custom website Md Sharik built for our startup is fantastic. It's modern, easy to navigate, and perfectly aligns with our business goals. Thank you for your exceptional work and support.",
+        "The custom website Rizwan built for our startup is fantastic. It's modern, easy to navigate, and perfectly aligns with our business goals. Thank you for your exceptional work and support.",
     },
   ];
 };
