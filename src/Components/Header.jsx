104505507b22225e3054beecf63a9e81037d884b
rizwan3749 <firsindiacredit786@gmail.com>
2024-10-25 22:18:49 +0530

    r

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
     <div className="min-h-16 w-[90vw] smd:w-[60vw] bg-white/80 backdrop-blur-md px-4 border py-4 text-black rounded-full translate-x-[-50%] translate-y-[-50%] top-[4rem] left-[50%] fixed flex z-50 justify-between">
       <div className="h-10 w-40 flex gap-6 items-center">
         <div className="rounded-full border transition-all hover:scale-105 cursor-pointer border-zinc-300 min-w-10 min-h-10 font-bold flex items-center justify-center bg-yellow-300">
-          N.
+          R.
         </div>
         <div className="text-zinc-200">|</div>
         <div className="cursor-pointer">Home</div>
